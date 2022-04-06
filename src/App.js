3c6249ff0aa7af19d67c77aee1a931a14f6c4c0c
sito8943 <sito8943@gmail.com>
2022-04-06 10:21:21 -0400

    loading screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
+/* eslint-disable react/function-component-definition */
+import { useState, useEffect } from "react";
+
+// react-lottie
+import Lottie from "react-lottie";
+
 // codememe2share components
-import MemeShare, { Container, Rotate } from "codememe2share";
+import MemeShare, {
+  Loading,
+  Container,
+  Rotate,
+  AppleDots,
+  LinkButton,
+  Title,
+  Paragraph,
+} from "codememe2share";
+
+// icons
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 // images
+import lottie from "assets/images/loading.json";
 import logo from "assets/images/logo.svg";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log(window.navigator.language);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
   return (
-    <MemeShare>
+    <MemeShare background="random">
+      <Loading
+        style={{
+          transition: "all 1000 ease",
+          opacity: loading ? 1 : 0,
+          zIndex: loading ? 99 : -1,
+          background: "#222333",
+        }}
+      >
+        <Container style={{ width: 200, background: "none", backdropFilter: "none" }}>
+          <Lottie options={{ animationData: lottie }} />
+        </Container>
+      </Loading>
       <Container>
-        <Rotate>
+        <AppleDots />
+        <Rotate style={{ width: 250, margin: "auto" }} delay="5s">
           <img src={logo} alt="react-logo" />
         </Rotate>
+        <Title variant="h3">CodeMeme2Share</Title>
+        <Paragraph style={{ textAlign: "center" }}>
+          Do you want to make your cool code shorts public?
+        </Paragraph>
+        <LinkButton
+          link="/docs"
+          style={{
+            margin: "auto",
+            width: 100,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          Get started
+          <BsFillArrowRightCircleFill />
+        </LinkButton>
       </Container>
     </MemeShare>
   );
-}
+};
 
 export default App;
