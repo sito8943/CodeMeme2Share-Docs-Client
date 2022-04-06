7e5d676a769064329b497ddffd1a8101c674a765
sito8943 <sito8943@gmail.com>
2022-04-06 08:52:33 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-// app
-import App from "./App";
-
-// styles
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
