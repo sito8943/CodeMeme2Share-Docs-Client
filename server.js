e1d7ee7bbaf302c98a0322d5587464580d57cf12
sito8943 <sito8943@gmail.com>
2022-04-08 15:58:58 -0400

    port 9000

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { docs } = require("./users/users");
 const { unauthorizedResponse } = require("./users/functions");
 
 const app = express();
+const port = 9000;
 
 app.use(cors());
 app.use(express.json({ limit: 1048576 }));
@@ -27,4 +28,6 @@ app.post("/file", (req, res) => {
   res.send(markdown);
 });
 
-app.listen(9000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
