b91a541bee0bdc9ee7ad4d63d86680af3d1b3101
sito8943 <sito8943@gmail.com>
2022-04-09 09:54:50 -0400

    server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(basicAuth({ users: docs, unauthorizedResponse }));
 
 // allow cross-origin requests
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
