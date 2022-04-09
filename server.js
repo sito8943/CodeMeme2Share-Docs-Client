64dbc7b8fb7b9ae1e41a9feb1f3856eb7282f929
sito8943 <sito8943@gmail.com>
2022-04-09 09:17:58 -0400

    cors crossorigin

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "build")));
 app.use(basicAuth({ users: docs, unauthorizedResponse }));
 
+// allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
