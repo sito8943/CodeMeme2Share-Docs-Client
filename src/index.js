0506cad9257b81d4229076b986f80d82813287a4
sito8943 <sito8943@gmail.com>
2022-04-10 13:04:11 -0400

    preparing example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
-// context
-import { LanguageProvider } from "context/Language";
-import { IndexProvider } from "context/IndexContext";
-
 // views
 import App from "./App";
 
@@ -16,10 +12,6 @@ const root = ReactDOMClient.createRoot(container);
 // Initial render: Render an element to the root.
 root.render(
   <StrictMode>
-    <IndexProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </IndexProvider>
+    <App />
   </StrictMode>
 );
--- src/lang/lang.js
+++
@@ -1,9 +0,0 @@
-import en from "./en.json";
-import es from "./es.json";
-
-const languages = {
-  en: en.en,
-  es: es.es,
-};
-
-export default languages;
